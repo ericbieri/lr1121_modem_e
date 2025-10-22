ec819d60af62b6013aaedd06854ac5d5ed3ef1af
Erich Bieri <erich.bieri@post.ch>
2025-10-22 13:27:21 +0200

    refactored modem_hal_wakeup

--- src/lr1121_modem_hal.cpp
+++ src/lr1121_modem_hal.cpp
@@ -56,18 +56,12 @@ lr1121_modem_hal_status_t lr1121_modem_hal_wakeup(const void* context) {
 
     const lr1121_modem_hal_context_t* ctx = (const lr1121_modem_hal_context_t*)context;
     
-    // If busy = HIGH
-    if (lr1121_modem_hal_wait_on_busy(context, LR1121_MODEM_WAKEUP_TIMEOUT_MS, HIGH) == LR1121_MODEM_HAL_STATUS_OK) {
-        // Wakeup radio by toggling CS pin
-        digitalWrite(ctx->cs_pin, LOW);
-        delayMicroseconds(LR1121_MODEM_WAKEUP_PULSE_DURATION_US); // Ensure CS is low for at least 100us 
-        digitalWrite(ctx->cs_pin, HIGH);
-    } else {
-        return LR1121_MODEM_HAL_STATUS_BUSY_TIMEOUT;
+    // Wait untill busy = HIGH to wake up the chip
+    if ((lr1121_modem_hal_wait_on_busy(context, LR1121_MODEM_WAKEUP_TIMEOUT_MS, HIGH) == LR1121_MODEM_HAL_STATUS_OK) &&
+        (lr1121_hal_wakeup(context) == LR1121_HAL_STATUS_OK)) {
+        return LR1121_MODEM_HAL_STATUS_OK;
     }
-    
-    // Wait for busy = LOW
-    return lr1121_modem_hal_wait_on_busy(context, LR1121_MODEM_WAIT_ON_BUSY_MS, LOW);
+    return LR1121_MODEM_HAL_STATUS_BUSY_TIMEOUT;
 }
 
 /*!
