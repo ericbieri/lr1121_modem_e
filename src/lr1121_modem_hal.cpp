19ec75b12d84e03203cc301a9e76f46043b65c7b
Erich Bieri <erich.bieri@post.ch>
2025-10-25 20:51:47 +0200

    Precalculate CRC before sending

--- src/lr1121_modem_hal.cpp
+++ src/lr1121_modem_hal.cpp
@@ -102,6 +102,10 @@ lr1121_modem_hal_status_t lr1121_modem_hal_write( const void* context, const uin
         // Select chip
         digitalWrite(ctx->cs_pin, LOW);
 
+        // Compute CRC
+        crc = lr1121_modem_compute_crc(0xFF, command, command_length);
+        crc = lr1121_modem_compute_crc(crc, data, data_length);
+
         // Send command
         for (uint16_t i = 0; i < command_length; i++) {
             SPI.transfer(command[i]);
@@ -110,11 +114,6 @@ lr1121_modem_hal_status_t lr1121_modem_hal_write( const void* context, const uin
         for (uint16_t i = 0; i < data_length; i++) {
             SPI.transfer(data[i]);
         }
-
-        // Compute and send CRC
-        crc = lr1121_modem_compute_crc(0xFF, command, command_length);
-        crc = lr1121_modem_compute_crc(crc, data, data_length);
-
         // Send CRC
         SPI.transfer(crc);
 
@@ -134,12 +133,12 @@ lr1121_modem_hal_status_t lr1121_modem_hal_write( const void* context, const uin
         // Send dummy bytes
         status = (lr1121_modem_hal_status_t) SPI.transfer(0x00);
         crc_received = (lr1121_modem_hal_status_t) SPI.transfer(0x00);
-        
-        // Compute response CRC
-        crc = lr1121_modem_compute_crc(0xFF, (uint8_t*)&status, 1);
-        
+    
         // Deselect chip
         digitalWrite(ctx->cs_pin, HIGH);
+
+        // Compute response CRC
+        crc = lr1121_modem_compute_crc(0xFF, (uint8_t*)&status, 1);
 
         if (crc != crc_received) {
             // Change the response code
@@ -181,18 +180,18 @@ lr1121_modem_hal_status_t lr1121_modem_hal_write_without_rc( const void* context
         // Select chip
         digitalWrite(ctx->cs_pin, LOW);
 
+        // Compute CRC
+        crc = lr1121_modem_compute_crc(0xFF, command, command_length);
+        crc = lr1121_modem_compute_crc(crc, data, data_length);
+
         // Send command
         for (uint16_t i = 0; i < command_length; i++) {
             SPI.transfer(command[i]);
         }
-
         // Send data
         for (uint16_t i = 0; i < data_length; i++) {
             SPI.transfer(data[i]);
         }
-        // Compute and send CRC
-        crc = lr1121_modem_compute_crc(0xFF, command, command_length);
-        crc = lr1121_modem_compute_crc(crc, data, data_length);
         // Send CRC
         SPI.transfer(crc);
 
@@ -230,13 +229,15 @@ lr1121_modem_hal_status_t lr1121_modem_hal_read(const void* context, const uint8
         // Select chip
         digitalWrite(ctx->cs_pin, LOW);
 
+        // Compute CRC
+        crc = lr1121_modem_compute_crc(0xFF, command, command_length);
+
         // Send command
         for (uint16_t i = 0; i < command_length; i++) {
             SPI.transfer(command[i]);
         }
 
-        // Compute and send CRC
-        crc = lr1121_modem_compute_crc(0xFF, command, command_length);
+        // Send CRC 
         SPI.transfer(crc);
 
         // Deselect chip
@@ -258,6 +259,7 @@ lr1121_modem_hal_status_t lr1121_modem_hal_read(const void* context, const uint8
                 data[i] = SPI.transfer(0);
             }
         }
+        // Read CRC
         crc_received = SPI.transfer(0);
 
         // Deselect chip
@@ -273,7 +275,7 @@ lr1121_modem_hal_status_t lr1121_modem_hal_read(const void* context, const uint8
         if (status == LR1121_MODEM_HAL_STATUS_OK) {
             crc = lr1121_modem_compute_crc(crc, data, data_length);
         }
-
+        // Compare CRCs
         if (crc != crc_received) {
             // Change the response code
             status = LR1121_MODEM_HAL_STATUS_BAD_FRAME;
